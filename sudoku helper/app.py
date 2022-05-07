efc5ebb49f06b7f336d24b0097ab54efb3fa0fd8
YLashko <79853449+YLashko@users.noreply.github.com>
2022-05-07 14:17:39 +0200

    Delete app.py

--- sudoku helper/app.py
+++
@@ -1,5 +0,0 @@
-for x in range(9):
-    print('<tr>')
-    for y in range(10):
-        print(f'    <td class="sudoku-number" id="number-{x}-{y}" onclick="incNumber(this, {x}, {y})"></td>' if y != 9 else f'    <td class="sudoku-choose-number" id="sudoku-choose-number-{x}" onclick="chooseNumber(this, {x + 1})">{x + 1}</td>')
-    print('</tr>')
